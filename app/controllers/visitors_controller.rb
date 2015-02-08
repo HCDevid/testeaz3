2c8d79da42de847e6d1b3a5c2a461c16cccca5e0
limsoup <limsoup@gmail.com>
2015-02-07 20:19:31 -0500

    making assets upload

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,6 @@
 class VisitorsController < ApplicationController
+
+  def google_verify
+    render 'google908b5ad4963c9554', :layout => false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  get '/google908b5ad4963c9554', to: 'visitors#google_verify'
 end
